118353f4373bbd49f628a1d05033c60767d35520
akash <akashtupe003@gmail.com>
2026-02-19 20:04:18 +0530

    fff

--- airflow/pyspark_job.py
+++ airflow/pyspark_job.py
@@ -14,13 +14,14 @@ CLUSTER_NAME = "cluster-2"
 COMPOSER_BUCKET = "us-central1-healthcare-comp-05a9bc1b-bucket"
 
 GCS_JOB_FILE_1 = f"gs://{COMPOSER_BUCKET}/data/etl/load_data.py"
+GCS_JOB_FILE_2 = f"gs://{COMPOSER_BUCKET}/data/etl/load_claim_data.py"
+
 PYSPARK_JOB_1 = {
     "reference": {"project_id": PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
     "pyspark_job": {"main_python_file_uri": GCS_JOB_FILE_1},
 }
 
-GCS_JOB_FILE_2 = f"gs://{COMPOSER_BUCKET}/data/etl/load_claim_data.py"
 PYSPARK_JOB_2 = {
     "reference": {"project_id": PROJECT_ID},
     "placement": {"cluster_name": CLUSTER_NAME},
@@ -29,40 +30,42 @@ PYSPARK_JOB_2 = {
 
 ARGS = {
     "owner": "airflow",
-    "start_date": None,
+    "start_date": days_ago(1),
     "depends_on_past": False,
     "retries": 1,
-    "retry_delay": timedelta(minutes=5)
+    "retry_delay": timedelta(minutes=5),
 }
 
-# define the dag
 with DAG(
     dag_id="pyspark_dag",
     schedule_interval=None,
-    description="DAG to start a Dataproc cluster, run PySpark jobs, and stop the cluster",
     default_args=ARGS,
-    tags=["pyspark", "dataproc", "etl"]
+    catchup=False,
+    tags=["pyspark", "dataproc"],
 ) as dag:
-    
+
     start_cluster = DataprocStartClusterOperator(
         task_id="start_cluster",
         project_id=PROJECT_ID,
         region=REGION,
         cluster_name=CLUSTER_NAME,
+        deferrable=False,
     )
 
     pyspark_task_1 = DataprocSubmitJobOperator(
-        task_id="pyspark_task_1", 
-        job=PYSPARK_JOB_1, 
-        region=REGION, 
-        project_id=PROJECT_ID
+        task_id="pyspark_task_1",
+        job=PYSPARK_JOB_1,
+        region=REGION,
+        project_id=PROJECT_ID,
+        deferrable=False,
     )
 
     pyspark_task_2 = DataprocSubmitJobOperator(
-        task_id="pyspark_task_2", 
-        job=PYSPARK_JOB_2, 
-        region=REGION, 
-        project_id=PROJECT_ID
+        task_id="pyspark_task_2",
+        job=PYSPARK_JOB_2,
+        region=REGION,
+        project_id=PROJECT_ID,
+        deferrable=False,
     )
 
     stop_cluster = DataprocStopClusterOperator(
@@ -70,6 +73,7 @@ with DAG(
         project_id=PROJECT_ID,
         region=REGION,
         cluster_name=CLUSTER_NAME,
+        deferrable=False,
     )
 
-start_cluster >> pyspark_task_1 >> pyspark_task_2 >> stop_cluster
+    start_cluster >> pyspark_task_1 >> pyspark_task_2 >> stop_cluster
